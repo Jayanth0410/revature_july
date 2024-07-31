ed84495ad7be103a0e68a953cd9d69d242d855c5
JAYANTH KUMAR REDDY <JAYANTH KUMAR REDDY@DESKTOP-60SNDQA>
2024-07-31 09:39:06 +0530

    First Commit

---
+++ Snapchat/src/main/java/com/Snapchat/View/SnapchatView.java
@@ -0,0 +1,19 @@
+package com.Snapchat.View;
+import java.util.*;
+
+public class SnapchatView {
+
+	public static void main(String[] args) {
+		System.out.println("***** Main Menu ******");
+		System.out.println("Press 1 to Login");
+		System.out.println("Press 2 to Capture");
+		System.out.println("Press 3 to share");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int a=sc.nextInt();
+		switch(a){
+		
+
+	}
+
+}
